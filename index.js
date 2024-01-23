ed7dd5e2af9eeaf8d1cb59e5ab337c29d533f1c0
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2024-01-23 11:00:31 +0530

    finalize backend structure

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
+import express from "express";
+import bodyParser from "body-parser";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from "dotenv";
 dotenv.config();
 
 const apiKEY = process.env.API_KEY;
+const PORT = process.env.PORT || 3001;
 
-const genAI = new GoogleGenerativeAI(apiKEY);
+const app = express();
 
-async function run() {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+app.use(bodyParser.json());
 
-  const prompt = "Write a story about Sri Lanka.";
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-  const result = await model.generateContent(prompt);
-  const response = result.response;
-  const text = response.text();
-  console.log(text);
-}
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome To GoTrip Server 🤗</h1>").status(200);
+});
 
-run();
+app.get("/status", (req, res) => {
+  res
+    .json({
+      status: "Up And Running...",
+    })
+    .status(500);
+});
+
+app.post("/generate", async (req, res) => {
+  try {
+    const { advice } = await req.body;
+
+    // Initiate the AI model
+    const genAI = new GoogleGenerativeAI(apiKEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = advice;
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    res.status(200).json(response.text());
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on PORT: ${PORT}`);
+});
